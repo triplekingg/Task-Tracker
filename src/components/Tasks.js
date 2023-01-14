9ad135b633ea9d437e8b0e6143e2b660337496ef
triplekingg <tripleking123@gmail.com>
2023-01-14 08:56:54 +0700

    Create separate components for task and tasks

--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -1,29 +1,12 @@
-const tasks = [
-    {
-        id:1,
-        text: 'Doctors Appointment',
-        day: 'Feb 5th at 2:30pm',
-        reminder: true,
-    },
-    {
-        id:2,
-        text: 'Meeting at School',
-        day: 'Feb 6th at 1:30pm',
-        reminder: true,
-    },
-    {
-        id:3,
-        text: 'Food Shopping',
-        day: 'Feb 5th at 2:30pm',
-        reminder: false,
-    }
-]
+import Task from './Task'
 
-const Tasks = () => {
-  return (
-    <>
-    {tasks.map((task) => (<h3>{task.text}</h3>))}
-    </>
+const Tasks = ({tasks}) => {
+    return (
+        <>
+        {tasks.map((task) => (
+         <Task key={task.id} task={task} />
+        ))}
+        </>
   )
 }
 
