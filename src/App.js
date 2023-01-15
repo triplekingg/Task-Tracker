786dbdf2e5b70f31a37003c5fa654e19fc9b46b7
triplekingg <tripleking123@gmail.com>
2023-01-15 10:29:08 +0700

    enable toggle reminder on server

--- src/App.js
+++ src/App.js
@@ -25,6 +25,14 @@ const fetchTasks = async () => {
   return data
 }
 
+//Fetch Task
+const fetchTask = async (id) => {
+  const res = await fetch(`http://localhost:5000/tasks/${id}`)
+  const data = await res.json()
+
+  return data
+}
+
 //Add Task
 const addTask = async (task) => {
   const res = await fetch('http://localhost:5000/tasks',{
@@ -52,8 +60,21 @@ const deleteTask = async (id) => {
 }
 
 //Toggle Reminder
-const toggleReminder = (id) => {
-  setTasks(tasks.map((task) => task.id === id ? { ...task, reminder: !task.reminder} : task))
+const toggleReminder = async(id) => {
+  const taskToToggle = await fetchTask(id)
+    const updTask = { ...taskToToggle, reminder: !taskToToggle.reminder }
+
+    const res = await fetch(`http://localhost:5000/tasks/${id}`, {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(updTask),
+    })
+
+  const data = await res.json()
+
+  setTasks(tasks.map((task) => task.id === id ? { ...task, reminder: data.reminder} : task))
 }
 
   return (
