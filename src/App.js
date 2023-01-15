cbc8f575a5bfab6b59bd9174a15fdc1cbe3e1063
triplekingg <tripleking123@gmail.com>
2023-01-15 09:49:21 +0700

    show tasks when submitting form

--- src/App.js
+++ src/App.js
@@ -25,6 +25,13 @@ function App() {
     }
 ])
 
+//Add Task
+const addTask = (task) => {
+  const id = Math.floor(Math.random() * 10000) + 1
+  const newTask = {id, ...task}
+  setTasks([...tasks, newTask])
+}
+
 //Delete Task
 const deleteTask = (id) => {
   setTasks(tasks.filter((task) => task.id !== id))
@@ -38,7 +45,7 @@ const toggleReminder = (id) => {
   return (
     <div className="container">
       <Header />
-      <AddTask />
+      <AddTask onAdd={addTask} />
       {tasks.length > 0 ? <Tasks tasks={tasks} 
       onDelete={deleteTask} onToggle = {toggleReminder}/> : 'No Tasks To Show'}
     </div>
